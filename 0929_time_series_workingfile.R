cd518d748fe1bc3375ac22b3d7bc05c294a288ce
OliverPan <oliverpanbiz@OliverPans-Air.attlocal.net>
2021-09-30 21:49:41 -0700

    Oliver - outline

--- 0929_time_series_workingfile.R
+++ 0929_time_series_workingfile.R
@@ -1,5 +1,23 @@
 ### Hoya 7 Time Series Project - working file
-# Oliver is testing
+#### Please write your code under the corresponding section 
 
 data <- read.csv('/Users/oliverpanbiz/Documents/hoya7capstone/usliquorsales.csv')
 head(data)
+
+
+
+
+########## 1. Data Exploration
+
+########## 2. Stationarize the series
+
+########## 3. Plot ACF/PACF charts and find optimal parameters
+
+########## 4. Build the ARIMA model
+
+########## 5. Make predictions
+
+
+
+
+
