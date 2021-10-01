e23757f495eea68aee62bff1246a6e488f2a84c6
OliverPan <oliverpanbiz@OliverPans-Air.attlocal.net>
2021-09-30 22:26:13 -0700

    Stuck on date conversion

--- 0929_time_series_workingfile.R
+++ 0929_time_series_workingfile.R
@@ -1,13 +1,41 @@
 ### Hoya 7 Time Series Project - working file
 #### Please write your code under the corresponding section 
 
-data <- read.csv('/Users/oliverpanbiz/Documents/hoya7capstone/usliquorsales.csv')
-head(data)
+#libraries
+library(ggplot2)
+library(forecast)
+library(grid)
+library(Amelia)
+library(tseries)
+library(scales)
+library(gridExtra)
+library(lmtest)
+library(Rcpp)
 
-
+# read data
+tsdata <- read.csv('/Users/oliverpanbiz/Documents/hoya7capstone/usliquorsales.csv'
+                   ,header =TRUE)
+summary(tsdata)
+names(tsdata)
+head(tsdata)
 
 
 ########## 1. Data Exploration
+
+#Missing data check
+sum(is.na(tsdata))
+# no missing data
+
+#Convert the date field from character to date type
+tsdata$Period <- as.Date(tsdata$Period, "%m/%d/%Y")
+head(tsdata)
+
+#exploratory plot
+ts_plot <- ggplot(tsdata, aes(Period,Value)) + geom_line(na.rm=TRUE) + 
+  xlab("Month") + ylab("Sales") + 
+  scale_x_date(labels = date_format(format= "%b-%Y"),breaks = date_breaks("1 year")) + 
+  stat_smooth(colour = "green")
+ts_plot
 
 ########## 2. Stationarize the series
 
