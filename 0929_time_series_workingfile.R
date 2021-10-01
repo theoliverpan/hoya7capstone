123d49b94f95c68ab7c9c7c247bcc721280c7427
OliverPan <oliverpanbiz@OliverPans-Air.attlocal.net>
2021-10-01 10:52:47 -0700

    new plot

--- 0929_time_series_workingfile.R
+++ 0929_time_series_workingfile.R
@@ -30,10 +30,23 @@ tsdata$Period <- as.Date(tsdata$Period, format="%m/%d/%y")
 head(tsdata)
 
 #exploratory plot
-ts_plot <- ggplot(auto, aes(Period,Value)) + geom_line(na.rm=TRUE) + 
-  xlab("Month") + ylab("Auto Sales in Thousands") + 
-  scale_x_date(labels = date_format(format= "%b-%Y"),breaks = date_breaks("1 year")) + 
+#oliver: my plot looks weird feel free to take a stab
+ts_plot <- ggplot(tsdata, aes(Period,Value)) + geom_line(na.rm=TRUE) + 
+  xlab("Month") + ylab("alc. ") + 
+  #scale_x_date(labels = date_format(format= "%b-%Y"),breaks = date_breaks("1 month")) + 
   stat_smooth(colour = "green")
+
+ts_plot
+
+
+#conver to ts
+
+#plot with trendline
+plot(tsdata, col = "blue", main = "Alc Time Series Data")
+abline(reg=lm(tsdata~time(tsdata)), col="lightgray") #plotting the trend line
+
+plot(auto_ts, col = "blue", main = "Auto Sales Time Series Data")
+abline(reg=lm(auto_ts~time(auto_ts)), col="lightgray") #plotting the trend line
 
 ########## 2. Stationarize the series
 
