93753416ab8d342ec05112e9e359ee05b591dfd3
OliverPan <oliverpanbiz@OliverPans-Air.attlocal.net>
2021-10-01 21:46:26 -0700

    consolidated with Dani's code

--- 0929_time_series_workingfile.R
+++ 0929_time_series_workingfile.R
@@ -40,7 +40,7 @@ ts_plot
 
 
 #conver to ts
-cleaned_tsdata <-ts(tsdata[,c('Value')],frequency=12)
+cleaned_tsdata <-ts(tsdata[,c('Value')])
 class(cleaned_tsdata)
 summary(cleaned_tsdata)
 
@@ -68,11 +68,23 @@ gglagplot(cleaned_tsdata, set.lags=1:16)
 component.tsm2 = decompose(cleaned_tsdata, type="multiplicative", filter=NULL)
 plot(component.tsm2)
 
+
 # clean data
-class(tsdata)
+cleaned_tsdata1 <-ts(tsdata[,c('Value')])
 
+class(cleaned_tsdata1)
+tsdata$lsales <-tsclean(cleaned_tsdata1)
 
-tsdata$lsales <-tsclean(unlist(cleaned_tsdata))
+#Plot the cleaned data
+c_ts_plot <- ggplot(tsdata, aes(Period,lsales)) + geom_line(na.rm=TRUE) + 
+  xlab("Month") + ylab(" Sales in Thousands") + 
+  scale_x_date(labels = date_format(format= "%b-%Y"),breaks = date_breaks("1 year")) + 
+  stat_smooth(colour="green")
+c_ts_plot
+
+#compare cleaned vs. uncleaned plots
+grid.arrange(ts_plot,c_ts_plot,ncol=1, top = textGrob("Uncleaned vs Cleaned Series"))
+
 
 #plot
 c_ts_plot <- ggplot(tsdata, aes(Period,lsales)) + geom_line(na.rm=TRUE) +
@@ -80,9 +92,48 @@ c_ts_plot <- ggplot(tsdata, aes(Period,lsales)) + geom_line(na.rm=TRUE) +
   scale_x_date(labels = date_format(format= "%b-%Y"),breaks = date_breaks("1 year")) + stat_smooth(colour="green")
 c_ts_plot
 
+#We can remove the trend simply by differencing the data
+alc_dif <-diff(cleaned_tsdata1)
+autoplot(alc_dif)
+
+Acf(alc_dif)
+Pacf(alc_dif)
 
 
-########## 4. Build the ARIMA model
+#Use the cleaned data set and check acf and pacf plots again
+my_ts <- ts(na.omit(tsdata$lsales), frequency = 12)
+plot(my_ts)
+
+component.ts2 = decompose(my_ts)
+plot(component.ts2)
+
+Acf(my_ts)
+Pacf(my_ts)
+#this didn't seem to do a whole lot
+
+
+########## 4. Build the ARIMA model, from Dani's code
+dfit1 <-arima(cleaned_tsdata, order=c(0,1,0))
+plot(residuals(dfit1))
+
+Acf(residuals(dfit1))
+Pacf(residuals(dfit1))
+
+#Because the seasonal pattern is strong and stable, 
+#we will want to use an order of seasonal differencing in the model. 
+#Before that let’s try only with one seasonal difference i.e ARIMA(0,0,0)(0,1,0)
+
+dfit2 <- arima(cleaned_tsdata, order =c(0,0,0), seasonal = list(order = c(0,1,0), period = 12))
+plot(residuals(dfit2))
+Acf(residuals(dfit2))
+Pacf(residuals(dfit2))
+
+#lets try and apply both seasonal and non-seasonal differencing, ARIMA(0,1,0)(0,1,0)[12]
+dfit3 <- arima(cleaned_tsdata, order =c(0,1,0), seasonal = list(order = c(0,1,0), period = 12))
+plot(residuals(dfit3))
+Acf(residuals(dfit3))
+Pacf(residuals(dfit3))
+
 
 ########## 5. Make predictions
 
